4a875732e5c76aa5669f3a54c731bd7bfc4b917e
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-03-14 23:18:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 // Allow requests from the frontend domain
 const allowedOrigins = [
+    'https://codeforge-frontend-production.up.railway.app',
     'https://codeforge-frontend.netlify.app', // Frontend hosted on Netlify
     'http://localhost:3000' // Local development
 ];
